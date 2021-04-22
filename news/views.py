af742fd6634a8e58fdb5aa7bf6e4d6eee86f81fd
Marleono <73365729+Marleono@users.noreply.github.com>
2021-04-22 09:26:47 +0300

    Update views.py

--- news/views.py
+++ news/views.py
@@ -7,9 +7,7 @@ from django.core.paginator import Paginator
 from datetime import datetime
 from django.views.generic import TemplateView
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.shortcuts import redirect
-from django.contrib.auth.models import Group
-from django.contrib.auth.decorators import login_required
+
 
 
 # Create your views here.
@@ -53,18 +51,3 @@ class PostDeleteView(DeleteView):
     success_url = '/news/'
 
 
-class IndexView(LoginRequiredMixin, TemplateView):
-    template_name = 'index.html'
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['is_not_author'] = not self.request.user.groups.filter(name='authors').exists()
-        return context
-
-@login_required
-def upgrade_me(request):
-    user = request.user
-    authors_group = Group.objects.get(name='authors')
-    if not request.user.groups.filter(name='authors').exists():
-        authors_group.user_set.add(user)
-    return redirect('/')

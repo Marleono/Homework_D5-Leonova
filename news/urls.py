7270e6abcc128ab3b9581372d5e629c1cbbea5b4
Marleono <73365729+Marleono@users.noreply.github.com>
2021-04-23 21:11:50 +0300

    Update urls.py

--- news/urls.py
+++ news/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from .views import Posts, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView
-from .views import upgrade_me
+
 
 urlpatterns = [
     path('', Posts.as_view()),
@@ -9,7 +9,7 @@ urlpatterns = [
     path('add/', PostCreateView.as_view(), name='news_add'), # Ссылка на создание товара
     path('<int:pk>/edit/', PostUpdateView.as_view(), name='news_add'),
     path('<int:pk>/delete/', PostDeleteView.as_view(), name='news_delete'),
-    path('upgrade/', upgrade_me, name = 'upgrade'),
+    
 
 
 ]
